a0be3a8b7d19b5e8758d0fd2671c7c248a5d010e
Instinct <maxcorreoimo@gmail.com>
2018-07-26 14:38:37 -0400

    new file:   testbd.py

---
+++ testbd.py
@@ -0,0 +1,2 @@
+from sqlalchemy import create_engine
+engine = create_engine('postgres://lcaojywwqecaor:0371754f3657c8a837db0dfea04c265ab5ce54c128ae23a611a5c4c920d752cf@ec2-54-83-22-244.compute-1.amazonaws.com:5432/d8g4bumo2vcgsn', echo=True)
